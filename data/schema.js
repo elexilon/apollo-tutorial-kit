e94a51b610a5230065180a0bd7c32efbef32e073
elexilon <elexilon@gmail.com>
2018-05-07 10:11:36 +0200

    modify typedefs

--- data/schema.js
+++ data/schema.js
@@ -3,8 +3,26 @@ import mocks from './mocks';
 
 const typeDefs = `
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String # we'll use this later
 }
+
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+
 `;
 
 const schema = makeExecutableSchema({ typeDefs });
